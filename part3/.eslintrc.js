943add5d486549ed60fbfcb2a84f9bf015b7e7d9
Fraz Aslam <faslam7646@gmail.com>
2023-05-18 21:14:39 -0400

    final changes, finished project

---
+++ part3/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "airbnb-base",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+  },
+};
--- part3/index.js
+++ part3/index.js
@@ -13,29 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 // entire list of all
 app.get("/api/persons", (request, response) => {
   PersonDB.find({}).then((person) => {
@@ -43,12 +20,14 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
+// get info on the amount of people are in the phonebook
 app.get("/api/info", (request, response) => {
   response.send(
-    `<tag> Phonebook has info for ${persons.length}</tag> people ${new Date()}`
+    `<tag> Phonebook has info for ${PersonDB.length}</tag> people ${new Date()}`
   );
 });
 
+//get an individual person from the database based on their id
 app.get("/api/persons/:id", (request, response, next) => {
   PersonDB.findById(request.params.id)
     .then((person) => {
@@ -62,6 +41,7 @@ app.get("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+// handle error
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
@@ -73,7 +53,7 @@ const errorHandler = (error, request, response, next) => {
 
   next(error);
 };
-
+// delete an item from database
 app.delete("/api/persons/:id", (request, response, next) => {
   PersonDB.findByIdAndRemove(request.params.id)
     .then((result) => {
@@ -81,13 +61,8 @@ app.delete("/api/persons/:id", (request, response, next) => {
       response.status(204).end();
     })
     .catch((error) => next(error));
-  //console.log(request);
-  //const id = Number(request.params.id);
-  //persons = persons.filter((person) => person.id !== id);
-
-  //response.status(204).end();
 });
-
+// create a person/number and add it to the database
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
@@ -97,7 +72,7 @@ app.post("/api/persons", (request, response, next) => {
     });
   }
 
-  const currentPeople = persons.map((person) => person.name);
+  const currentPeople = PersonDB.map((person) => person.name);
 
   if (currentPeople.includes(body.name)) {
     return response.status(400).json({
@@ -115,8 +90,6 @@ app.post("/api/persons", (request, response, next) => {
       response.json(savedPerson);
     })
     .catch((error) => next(error));
-  //persons = persons.concat(person);
-  //response.json(savedNote);
 });
 
 app.use(errorHandler);
